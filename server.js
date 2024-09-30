ebe201e8ee4b61e08174293f8498862463589c30
Afnan Khalid <afnank19@gmail.com>
2024-09-30 20:42:21 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const { CronJob } = require('cron')
+const express = require('express')
+const axios = require('axios');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test', async (req, res) => {
+    res.status(200).json({message: "Hope you didnt wait 5 minutes for the cold start ;)"})
+})
+
+const job = new CronJob(
+	'*/4 * * * *', // cronTime
+	async function () {
+		console.log('You will see this message every minute');
+        try {
+            const response = await axios.get('http://localhost:3000/test');
+            console.log('Cron Job: Response from /test:', response.data);
+        } catch (error) {
+            console.log(error)
+        }
+	}, // onTick
+	null, // onComplete
+	true, // start
+	'America/Los_Angeles' // timeZone
+);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
